76512ec3d8b7c3c3bfd35f751d98d68bc1a09ad2
Ryan DeGroot <degrootr253@gmail.com>
2022-01-04 12:16:58 -0800

    adding front end routes to index.js route folder

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require("express").Router();
 
 const apiRoutes = require('./api');
+const frontEnd = require('./frontEnd')
 
+router.use('/', frontEnd);
 router.use('/api', apiRoutes);
+
