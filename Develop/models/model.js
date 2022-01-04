20a9261971eb99643847c3134b1a12c21ceb250f
Ryan DeGroot <degrootr253@gmail.com>
2022-01-04 11:43:12 -0800

    adding elements to better fit exercise schema based on what exercise it is

--- Develop/models/model.js
+++ Develop/models/model.js
@@ -3,16 +3,30 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: "Enter a name for transaction"
-  },
-  value: {
-    type: Number,
-    required: "Enter an amount"
-  },
-  date: {
+
+    exercises: [{
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter exercise name"
+    },
+    distanceInput: {
+        type: Number
+      },
+    durationInput: {
+        type: Number
+    },
+    weightInput: {
+        type: Number
+    },
+   setsInput: {
+       type: Number
+   },
+   reps: {
+       type: Number
+   } 
+  }],
+  day: {
     type: Date,
     default: Date.now
   }
