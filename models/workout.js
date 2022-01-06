819f63fd617a95ce823303a1a95c6109f9d754f0
Ryan DeGroot <degrootr253@gmail.com>
2022-01-05 18:31:47 -0800

    update

--- models/workout.js
+++ models/workout.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
     exercises: [{
         name: {
             type: String,
@@ -25,11 +28,7 @@ const workoutSchema = new Schema({
         reps: {
             type: Number
         }
-    }],
-    day: {
-        type: Date,
-        default: () => new Date()
-    }
+    }]
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
